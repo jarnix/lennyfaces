79f2245e5945dab776cb72b208c8cb47135d2551
Julien Ricard <jarnix@gmail.com>
2016-02-11 00:18:13 +0100

    sdlkfjdsk

--- popup.js
+++ popup.js
@@ -31,19 +31,15 @@ for(var category in allItems) {
   $('section#content').append('<div class="category" id="' + category + '" style="display:none;">' + category + '</div>');
 }
 $("nav ul li").click(function(el) {
-
-
     $('section#content div.category').hide();
     $('section#content div.category#' + $(el.target).attr('data-category')).show();
-
 });
 
-/*
+// content
 for(var category in allItems) {
-
+  var html='';
   for(var item in allItems[category].items) {
     html+='<button class="' + category + ' item" text="' + allItems[category].items[item] + '">' + allItems[category].items[item] +'</button>';
   }
+  $('section#content div.category#' + category).html(html);
 }
-$('section#content').html(html);
-*/
